b1f45bc66c3877b977f99ae369d217c50134640e
Ivan Limongan <ivan.limongan@gmail.com>
2016-04-21 20:09:12 -0700

    adjusted photos and structure

--- photos.js
+++ photos.js
@@ -5,46 +5,46 @@ var Photo = function(imagepath, name, neighborhoods) { //object constructor for 
 }
 
 
-var wakeboarding = new Photo("wakeboarding.jpg", "wakeboarding", [n, ne]);
-var snowboarding = new Photo("snowboarding.jpg", "snowboarding", [ne, se]); // consider replacing with horse shoeing
-var skiing = new Photo("skiing.jpg", "skiing", [nw, sw]);
-var hiking = new Photo("hiking.jpg", "hiking" [n, se, ne]);
+var wakeboarding = new Photo("images/Activity/wakeboarding.jpg", "wakeboarding", [n, ne]);
+var snowboarding = new Photo("images/Activity/snowboarding.jpg", "snowboarding", [ne, se]); // consider replacing with horse shoeing
+var skiing = new Photo("images/Activity/skiing.jpg", "skiing", [nw, sw]);
+var hiking = new Photo("images/Activity/hiking.jpg", "hiking" [n, se, ne]);
 
-var voodoo = new Photo("voodoo.jpg", "voodoo", [sw, ne]);
-var bluestar = new Photo("bluestar.jpg", "bluestar", [nw, se]);
-var oldfashioned = new Photo("oldfashioned.jpg", "oldfashioned", [n, ne]);
-var pips = new Photo("pips.jpg", "pips", [ne]);
+var voodoo = new Photo("images/Donuts/voodoo.jpg", "voodoo", [sw, ne]);
+var bluestar = new Photo("images/Donuts/bluestar.jpg", "bluestar", [nw, se]);
+var oldfashioned = new Photo("images/Donuts/oldfashioned.jpg", "oldfashioned", [n, ne]);
+var pips = new Photo("images/Donuts/pips.jpg", "pips", [ne]);
 
-var latte = new Photo("latte.jpg", "latte", [nw, sw]);
-var iced = new Photo("iced.jpg", "iced", [sw]);
-var tea = new Photo("tea.jpg", "tea", [ne, se]);
-var americano = new Photo("americano.jpg", "americano", [nw, n]);
-var frapp = new Photo("frapp.jpg", "frapp", [sw]);
+var latte = new Photo("images/Coffee/latte.jpg", "latte", [nw, sw]);
+var iced = new Photo("images/Coffee/iced.jpg", "iced", [sw]);
+var tea = new Photo("images/Coffee/tea.jpg", "tea", [ne, se]);
+var americano = new Photo("images/Coffee/americano.jpg", "americano", [nw, n]);
+var frapp = new Photo("images/Coffee/frapp.jpg", "frapp", [sw]);
 
-var sportsbar = new Photo("images/sportsbar.jpg", "sportsbar", [ne, n]);
-var club = new Photo("images/club.jpg", "club", [nw, sw]);
-var bartender = new Photo("images/bartender.jpg", "bartender", [se, ne]);
+var sportsbar = new Photo("images/Night/sportsbar.jpg", "sportsbar", [ne, n]);
+var club = new Photo("images/Night/club.jpg", "club", [nw, sw]);
+var bartender = new Photo("images/Night/bartender.jpg", "bartender", [se, ne]);
 
-var pabst = new Photo("pabst.jpg", "pabst", [n]);
-var cocktails = new Photo("images/cocktails.jpg", "cocktails", [nw, sw]);
-var craftbeer = new Photo("craftbeer.jpg", "craftbeer", [ne, se]);
+var pabst = new Photo("images/Drink/pabst.jpg", "pabst", [n]);
+var cocktails = new Photo("images/Drink/ocktails.jpg", "cocktails", [nw, sw]);
+var craftbeer = new Photo("images/Drink/craftbeer.jpg", "craftbeer", [ne, se]);
 
-var home = new Photo("home.jpg", "home", [ne, se, sw]);
-var condo = new Photo("condo.jpg", "condo" [nw, sw]);
-var boathouse = new Photo("boathouse.jpg", "boathouse", [n, nw]);
+var home = new Photo("images/Housing/home.jpg", "home", [ne, se, sw]);
+var condo = new Photo("images/Housing/condo.jpg", "condo" [nw, sw]);
+var boathouse = new Photo("images/Housing/boathouse.jpg", "boathouse", [n, nw]);
 
-var blazer = new Photo("blazer.gif", "blazer", [ne, se]);
-var hops = new Photo("hops.jpg", "hops", [sw]);
-var thorns = new Photo("thorns.png", "thorns", [se, nw]);
-var timbers = new Photo("timbers.png", "timbers", [nw, sw]);
-var winterhawks = new Photo("winterhawks.png", "winterhawks", [ne, n]);
+var blazer = new Photo("images/Sports/blazer.gif", "blazer", [ne, se]);
+var hops = new Photo("images/Sports/hops.jpg", "hops", [sw]);
+var thorns = new Photo("images/Sports/thorns.png", "thorns", [se, nw]);
+var timbers = new Photo("images/Sports/timbers.png", "timbers", [nw, sw]);
+var winterhawks = new Photo("images/Sports/winterhawks.png", "winterhawks", [ne, n]);
 
-var walk = new Photo("walking.jpg", "walk", [nw, sw]);
-var car = new Photo("car.jpg", "car", [n, ne, se, sw]);
-var trimet = new Photo("trimet.jpg", "trimet", [n, ne, nw, sw, se]);
-var bike = new Photo("bike.jpg", "bike", [se, ne, nw]);
+var walk = new Photo("images/Transportation/walking.jpg", "walk", [nw, sw]);
+var car = new Photo("images/Transportation/car.jpg", "car", [n, ne, se, sw]);
+var trimet = new Photo("images/Transportation/trimet.jpg", "trimet", [n, ne, nw, sw, se]);
+var bike = new Photo("images/Transportation/bike.jpg", "bike", [se, ne, nw]);
 
-var suit = new Photo("suit.jpeg", "suit", [sw, nw]);
-var preppy = new Photo("preppy.png", "preppy", [sw]);
-var nerd = new Photo("nerd.png", "nerd", [ne, se, nw]);
-var hipster = new Photo("hipster.jpg", "hipster", [n, ne]);
+var suit = new Photo("images/Person/suit.jpeg", "suit", [sw, nw]);
+var preppy = new Photo("images/Person/preppy.png", "preppy", [sw]);
+var nerd = new Photo("images/Person/nerd.png", "nerd", [ne, se, nw]);
+var hipster = new Photo("images/Person/hipster.jpg", "hipster", [n, ne]);
